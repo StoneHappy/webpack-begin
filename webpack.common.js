5959f5f2310fc093414f32bd8b004d8f46296f4f
xhguleixin123 <18017158729@163.com>
2022-05-24 14:57:44 +0800

    ✨ feat: css extract

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,21 @@
 const path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: {
         main: "./src/index.js",
         vendor: "./src/vendor.js"
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html"
-        })
-    ],
     module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader", //3. Inject styles into DOM
-                    "css-loader", //2. Turns css into commonjs
-                    "sass-loader" //1. Turns sass into css
-                ]
-            },
-
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-
-            {
-                test: /\.(svg|png|jpg|gif)$/,
-                type: 'asset/resource'
-            }
-        ]
+      rules: [
+        {
+          test: /\.html$/,
+          use: ["html-loader"]
+        },
+        {
+          test: /\.(svg|png|jpg|gif)$/,
+          type: 'asset/resource'
+        }
+      ]
     }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
 
-const { CleanWebpackPlugin }= require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -10,6 +13,34 @@ module.exports = merge(common, {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "dist"),
     },
+    optimization: {
+        minimizer: [
+            new HtmlWebpackPlugin({
+                template: "./src/template.html",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true
+                }
+            })
+        ]
+    },
 
-    plugins: [new CleanWebpackPlugin()]
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+        new CleanWebpackPlugin()
+    ],
+
+    module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader, //3. Extract css into files
+              "css-loader", //2. Turns css into commonjs
+              "sass-loader" //1. Turns sass into css
+            ]
+          }
+        ]
+      }
 });
